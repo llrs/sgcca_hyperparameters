65a7c6ec6679e897b8527b0db29f39cffefb9e74
llrs <lluis.revilla@gmail.com>
2018-10-10 10:34:52 +0200

    Improve testing and models

--- testing_simulations.R
+++ testing_simulations.R
@@ -8,7 +8,7 @@ o <- create_dataset(5000, 250)
 i <- create_dataset(5000, 250)
 
 A <- list(o, o)
-sgcca.identical <- sgcca(A, matrix(c(0, 1, 1, 0), ncol =2 , nrow =2),
+sgcca.identical <- sgcca(A, matrix(c(0, 1, 1, 0), ncol = 2 , nrow = 2),
                           c1 = rep(tau.estimate(o), 2),
                           scheme = "centroid")
 plot(sgcca.identical$Y[[1]][, 1], sgcca.identical$Y[[2]][, 1])
@@ -19,7 +19,7 @@ plot(cmdscale(as.dist(o2)), pch = 16)
 
 
 A <- list(o, t(o))
-sgcca.transposed <- sgcca(A, matrix(c(0, 1, 1, 0), ncol =2 , nrow =2),
+sgcca.transposed <- sgcca(A, matrix(c(0, 1, 1, 0), ncol = 2 , nrow = 2),
                         c1 = c(tau.estimate(o), 0.142), # The minimum
                         scheme = "centroid")
 plot(sgcca.transposed$Y[[1]][, 1], sgcca.transposed$Y[[2]][, 1])
@@ -37,7 +37,7 @@ library("RGCCA")
 o <- create_dataset(5000, 500)
 i <- create_dataset(5000, 500)
 A <- list(t(o), t(i))
-C <- matrix(c(0, 1, 1, 0), ncol =2 , nrow =2)
+C <- matrix(c(0, 1, 1, 0), ncol = 2 , nrow = 2)
 tau <- rep(1/sqrt(500), 2)
 sgcca.diff <- sgcca(A, C, tau, scheme = "centroid")
 plot(sgcca.diff$Y[[1]][, 1], sgcca.diff$Y[[2]][, 1], pch = 16)
