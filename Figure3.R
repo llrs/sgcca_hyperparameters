9575642136a82599038589350dbd7689f7f4ace4
llrs <lluis.revilla@gmail.com>
2018-10-11 13:19:19 +0200

    Reproduce a figure for the paper

---
+++ Figure3.R
@@ -0,0 +1,26 @@
+library("ggpubr")
+
+superblock_interactions <- readRDS("superblock_interactions.RDS")
+weights_interactions <- readRDS("centroid_interactions.RDS")
+
+si <- superblock_interactions[,  c("AVE_inner", "var11")]
+wi <- weight_interactions[,  c("AVE_inner", "var11")]
+
+si <- cbind(si, Type = "Superblock")
+wi <- cbind(wi, Type = "Weights")
+df <- rbind.data.frame(si, wi)
+
+df$Interaction[df$var11 != 0] <- "Yes"
+df$Interaction[df$var11 == 0] <- "No"
+
+df$Classification <- paste0(df$Type, "-", df$Interaction)
+my_comparisons <- list(c("Superblock-No", "Superblock-Yes"),
+                       c("Weights-No", "Weights-Yes"),
+                       c("Superblock-No", "Weights-No"),
+                       c("Weights-Yes", "Superblock-Yes"))
+ggviolin(df, x = "Classification", y = "AVE_inner") +
+  stat_compare_means(comparisons = my_comparisons, paired = FALSE)+ # Add pairwise comparisons p-value
+  grids() +
+  ylab("inner AVE") +
+  xlab("Study-interaction") +
+  ggtitle("Effect of the interaction within GE")
