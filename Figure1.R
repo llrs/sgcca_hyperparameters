c000f8eec8a36a10efa94b4fac4c189210424a96
Lluís <lluis.revilla@gmail.com>
2020-10-30 16:25:42 +0100

    Add a variant of the plot as requested by reviewer 2

--- Figure1.R
+++ Figure1.R
@@ -4,18 +4,18 @@ library("patchwork")
 tau <- readRDS("factorial_tau.RDS")
 theme_set(theme_bw())
 comm0 <- ggplot(tau, aes(y, AVE_inner)) +
-  geom_point() +
   ylab("inner AVE") +
   xlab("tau y") +
   scale_fill_viridis_c()
-  scale_color_viridis_c()
 plot1 <- comm0 +
   geom_point(aes(fill = CGH, color = CGH)) +
+  geom_point(data = . %>% filter(GE == 0.503293099215374 & CGH == 0.636243186088126), col = "red") +
   guides(color = FALSE, fill = FALSE) +
   labs(title = "Colored by tau of CGH")
 plot2 <- comm0 +
   geom_point(aes(color = GE, fill = GE)) +
   guides(fill = FALSE) +
+  geom_point(data = . %>% filter(GE == 0.503293099215374 & CGH == 0.636243186088126), col = "red") +
   labs(title = "Colored by tau of GE", color = "tau") +
   theme( axis.text.y = element_blank(),
          axis.ticks.y = element_blank(),
@@ -25,6 +25,41 @@ p1
 ggsave("Figures/Figure1.png", plot = p1, width = 170, dpi = 300, units = "mm")
 
 
+# See the rank on the AVE_inner of those with closer to the tau.estimate
+factorial %>% arrange(-AVE_inner) %>%
+  mutate(n = row_number()) %>%
+  filter(GE == 0.503293099215374 & CGH == 0.636243186088126) %>%
+  filter(AVE_inner == max(AVE_inner)) %>%
+  # summarize(median(AVE_inner), median(AVE_outer), median(cc1)) %>%
+  print()
+# Number 97 is very good, the inner AVE median is slightly higher than those without.
+factorial %>% arrange(-AVE_inner) %>%
+  mutate(n = row_number()) %>%
+  filter(GE != 0.503293099215374 & CGH != 0.636243186088126) %>%
+  filter(AVE_inner == max(AVE_inner)) %>%
+  # summarize(median(AVE_inner), median(AVE_outer), median(cc1)) %>%
+  head()
+tau2 <- tau %>%
+  mutate(method = case_when(GE == 0.503293099215374 & CGH == 0.636243186088126 ~ "Schäfer's method",
+                            TRUE ~ "grid search"))
+p2 <- ggplot(tau2) +
+  geom_point(aes(GE, y, col = AVE_inner, shape = method)) +
+  labs(title = "by GE", shape = "Method", col = "inner AVE") +
+  scale_color_viridis_c(option = "A", direction = -1) +
+  ggplot(tau2) +
+  geom_point(aes(CGH, y, col = AVE_inner, shape = method)) +
+  scale_color_viridis_c(option = "A", direction = -1) +
+  labs(title = "by CGH", shape = "Method", col = "inner AVE") +
+  theme( axis.text.y = element_blank(),
+         axis.ticks.y = element_blank(),
+         axis.title.y = element_blank()) +
+  plot_layout(guides='collect') +
+  plot_annotation(tag_levels = "A", title = "Inner AVE depending on tau")
+# Following recommendation from reviewer 2.
+ggsave("Figures/Figure1b.tiff", plot = p2, width = 170, dpi = 300, units = "mm")
+
+group_by(tau2, GE, CGH, y) %>%
+  summarize(median(AVE_inner), median(AVE_outer))
 comm <- ggplot(tau) +
   scale_colour_viridis_c() +
   ylim(c(0.4, .7)) +
